bd99c39458d6b13165ed497a381d821cd019b129
root <root@AOXE-Nitro5.localdomain>
2023-03-26 12:17:53 -0300

    Aplicando Middleware de Auth na API

--- source/laravel/routes/api.php
+++ source/laravel/routes/api.php
@@ -19,19 +19,24 @@ use App\Http\Controllers\Api\Auth\AuthController;
 
 Route::post('/auth', [AuthController::class, 'auth']);
 
-Route::get('/courses', [CourseController::class, 'index']);
-Route::get('/courses/{id}', [CourseController::class, 'show']);
+//Middeware para filtrar as rotas, so sera acessadas, apois validar login
+Route::middleware(['auth:sanctum'])->group(function (){
 
-Route::get('/courses/{id}/modules', [ModuleController::class, 'index']);
+    Route::get('/courses', [CourseController::class, 'index']);
+    Route::get('/courses/{id}', [CourseController::class, 'show']);
+    
+    Route::get('/courses/{id}/modules', [ModuleController::class, 'index']);
+    
+    Route::get('/modules/{id}/lessons', [LessonController::class, 'index']);
+    Route::get('/lessons/{id}', [LessonController::class, 'show']);
+    
+    Route::get('/my-supports', [SupportController::class, 'mySupports']);
+    Route::get('/supports', [SupportController::class, 'index']);
+    Route::post('/supports', [SupportController::class, 'store']);
+    
+    Route::post('/replies', [ReplySupportController::class, 'createReply']);
 
-Route::get('/modules/{id}/lessons', [LessonController::class, 'index']);
-Route::get('/lessons/{id}', [LessonController::class, 'show']);
-
-Route::get('/my-supports', [SupportController::class, 'mySupports']);
-Route::get('/supports', [SupportController::class, 'index']);
-Route::post('/supports', [SupportController::class, 'store']);
-
-Route::post('/replies', [ReplySupportController::class, 'createReply']);
+});
 
 Route::get('/', function () {
     return response()->json([
