193ce8f7613a84c868d6683f149ebdc2ddeae33e
root <root@AOXE-Nitro5.localdomain>
2023-03-29 20:28:38 -0300

    Teste API Supports com Filtros

--- source/laravel/tests/Feature/Api/SupportTest.php
+++ source/laravel/tests/Feature/Api/SupportTest.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\Support;
+use App\Models\Lesson;
 
 class SupportTest extends TestCase
 {
@@ -23,7 +24,7 @@ class SupportTest extends TestCase
     {
         $user = $this->createUser();
         $token = $user->createToken('deviceTest')->plainTextToken;
-        
+
         Support::factory()->count(50)->create([
             'user_id' => $user->id
         ]);
@@ -36,5 +37,72 @@ class SupportTest extends TestCase
 
         $response->assertStatus(200)
                     ->assertJsonCount(50, 'data');
+    }
+
+    public function test_get_supports_unauthenticated()
+    {
+        $response = $this->getJson('/supports');
+
+        $response->assertStatus(401);
+    }
+
+    public function test_get_supports()
+    {
+        Support::factory()->count(50)->create();
+
+        $response = $this->getJson('/supports', $this->defaultHeaders());
+
+        $response->assertStatus(200)
+                    ->assertJsonCount(50, 'data');
+    }
+
+    public function test_get_supports_filter_lesson()
+    {
+        $lesson = Lesson::factory()->create();
+        Support::factory()->count(50)->create();
+        Support::factory()->count(10)->create([
+            'lesson_id' => $lesson->id,
+        ]);
+
+        $payload = [
+            'lesson' =>$lesson->id
+        ];
+
+        $response = $this->json('GET', '/supports', $payload, $this->defaultHeaders()); //Usando metodo json(), pois ele possibilita passar mais parametros do form via $payload
+
+        $response->assertStatus(200)
+                    ->assertJsonCount(10, 'data');
+    }
+
+    public function test_get_supports_filter_description()
+    {
+        $lesson = Lesson::factory()->create();
+        Support::factory()->create([
+            'lesson_id' => $lesson->id,
+        ]);
+
+        $payload = [
+            'filter' => $lesson->descricao
+        ];
+
+        $response = $this->json('GET', '/supports', $payload, $this->defaultHeaders()); //Usando metodo json(), pois ele possibilita passar mais parametros do form via $payload
+
+        $response->assertStatus(200);
+    }
+
+    public function test_get_supports_filter_status()
+    {
+        $lesson = Lesson::factory()->create();
+        Support::factory()->create([
+            'lesson_id' => $lesson->id,
+        ]);
+
+        $payload = [
+            'status' => 'P'
+        ];
+
+        $response = $this->json('GET', '/supports', $payload, $this->defaultHeaders()); //Usando metodo json(), pois ele possibilita passar mais parametros do form via $payload
+
+        $response->assertStatus(200);
     }
 }
